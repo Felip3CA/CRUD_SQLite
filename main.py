8dfa9eaba1db1226fcb0b262454dfc74cc7e2af9
Felipe <feliprcezaralmeida0@gmail.com>
2025-09-29 09:52:51 -0300

    Etapa 5 - Remoção de Livros

--- main.py
+++ main.py
@@ -49,17 +49,62 @@ cadastrar_livro(titulo, autor, ano)
 
 def listar_livros():
     try:
-        cursor.execute("SELECT * FROM alunos")
+        conexao = sqlite3.connect("biblioteca.db")
+        cursor = conexao.cursor()
+        cursor.execute("SELECT * FROM livros")
         #fetchall traz todas as linhas da consulta
         for linha in cursor.fetchall():
-            print(f"ID {linha[0]} | TÍTULO {linha[1]} | AUTOR {linha[2]} | ANO {linha[3]}")
+            print(f"ID {linha[0]} | TÍTULO {linha[1]} | AUTOR {linha[2]} | ANO {linha[3]} | DISPONIBILIDADE {linha[4]}")
 
-        livro = input("Qual curso você deseja ver os alunos: ")
-        #Selecionando apenas os alunos do curso Computação
-        cursor.execute("SELECT nome, idade FROM alunos WHERE curso = ?", (livro,) )
+        livro = input("Qual livro você deseja ver: ")
+
+        cursor.execute("SELECT nome, idade FROM livros WHERE curso = ?", (livro,) )
         for linha in cursor.fetchall():
             print(linha)
     except Exception as erro:
         print(f"Erro ao tentar listar os livros: {erro}")
 
-#Etapa 4 - Atualização de Disponibilidade:
+#Etapa 4 - Atualização de Disponibilidade: 
+
+
+
+
+#Etapa 5 - Remoção de Livros
+
+def deletar_livro(id_livro):
+    try:
+        # Conectando ao banco de dados
+        conexao = sqlite3.connect("biblioteca.db")
+        cursor = conexao.cursor()
+
+        # Deletando o livro com o ID fornecido
+        cursor.execute("DELETE FROM livros WHERE id = ?", (id_livro,))
+       
+        # Confirmando a alteração
+        conexao.commit()
+
+        # Verificando se algum livro foi realmente deletado
+        if cursor.rowcount > 0:
+            print("Livro removido com sucesso!")
+        else:
+            print("Nenhum livro encontrado com o ID fornecido.")
+
+    except Exception as erro:
+        # Caso ocorra algum erro relacionado ao banco de dados
+        print(f"Erro ao tentar excluir livro: {erro}")
+
+    finally:
+        # Sempre fechar a conexão, independentemente de sucesso ou erro
+        if conexao:
+            conexao.close()
+
+# Solicitando o ID do livro para deletar
+deletar = input("Digite o id do livro que deseja deletar: ")
+deletar_livro(deletar)
+
+# Etapa 6 - Menu Interativo:
+
+
+
+
+
